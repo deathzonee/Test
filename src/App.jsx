a4ce23665517e54a6277fbccf43f925b2ee0f810
Thanh Binh <binhzone111@gmail.com>
2024-02-28 22:24:30 +0700

    test

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Textarea } from "@material-tailwind/react";
+
+const App = () => {
+  return (
+    <div className="mt-[20px] w-96">
+      <Textarea color="red" variant="outlined" className="" label="Message" />
+    </div>
+  );
+};
+
+export default App;
